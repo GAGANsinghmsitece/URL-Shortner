f932b9668444587e412aa05e9ca86d72eb0fed77
Gagan Singh <gagan.s@smarter.codes>
2024-01-18 15:28:04 +0530

    update:- adding database

--- backend/src/helpers/db.js
+++ backend/src/helpers/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 let dbConnection;
 module.exports = {
     connectToDb: (cb) => {
-        mongoose.connect("mongodb://localhost:27017/url-shortener").then((client) => {
+        mongoose.connect(process.env.MONGO_URL).then((client) => {
             dbConnection = client;
             return cb();
         }).catch(err => {
